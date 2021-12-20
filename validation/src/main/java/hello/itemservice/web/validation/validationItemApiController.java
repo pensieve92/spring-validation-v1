4db212ed7af836c5aca7be550473734c992b16df
byeongjeon jeon <pensieve92@gmail.com>
2021-12-20 23:17:21 +0900

    13.Form 전송 객체 분리 - HTTP 메시지 컨버터 학습완료

---
+++ validation/src/main/java/hello/itemservice/web/validation/validationItemApiController.java
@@ -0,0 +1,29 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/itmes")
+public class validationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
+        log.info("API 컨트롤러 호충");
+
+        if (bindingResult.hasErrors()) {
+            log.info("검증 오류 발생", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("성공 로지 실행");
+        return form;
+    }
+}
